2c574532e9cf575e63c84c1febd27a81ceda809a
sourab <sourabmajhi.200@gmail.com>
2025-07-22 20:30:46 +0530

    livewire_fix

--- config/livewire.php
+++ config/livewire.php
@@ -2,6 +2,7 @@
 
 return [
 
+    'asset_url' => asset('vendor/livewire/livewire.js'),
     /*
     |---------------------------------------------------------------------------
     | Class Namespace
