2b19923e7bfd36d2ac2cc8948a807e07c5593e46
sourab <sourabmajhi.200@gmail.com>
2025-07-24 19:52:58 +0530

    livewire_custom_directive_handle

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -9,6 +9,80 @@ function random_str(length) {
   }
   return result;
 }
+const customDirectives = {
+       directives: [ ],
+
+       add: function(dir, fn ) {
+            this.directives[dir] = fn;
+       },
+       
+       init: function() {
+             this.deploy(this.directives);
+       },
+
+       deploy: function( directives ) {
+              
+              const elements = document.querySelectorAll(`[wr\\:loading]`);
+              var map = {};
+              elements.forEach(function( element, i){
+                  let target_methods_str  = element.getAttribute('wr:target')
+                  let target_methods  = target_methods_str? target_methods_str.split(',').map((method)=>method.trim()): [];
+                  var data  = { element, target_methods, hooks: []};
+                  Object.keys(directives ).forEach(( directive ) =>{
+                        const param = element.getAttribute(directive);
+                        if(param){
+                          data.hooks.push({method: directives[directive], param});
+                        }
+                  })
+                  
+                  target_methods.forEach(( method )=>{
+                        if(!(method in map)){
+                          map[ method ] = [];
+                        }
+                        map[method].push(data);
+                  })
+              })
+              Livewire.hook('commit', ({ component, commit, respond, succeed, fail }) => {
+                      var memo_hooks = [];
+                      var calls = commit.calls? commit.calls.map((c) => c.method): [];
+                      calls.forEach((call)=>{
+                          if(call in map){
+                              let elements = map[call];
+                              elements.forEach(({element, hooks})=>{
+                                  hooks.forEach((hook) =>{
+                                      memo_hooks.push({ element,...hook})
+                                  });
+                              })
+                          }
+                      })
+                      memo_hooks.forEach((hook)=> hook.method(hook.element, hook.param, 'start'));
+
+                      respond((params)=>{
+                            memo_hooks.forEach((hook)=> hook.method(hook.element, hook.param, 'finish'));
+                      })
+                      
+                      succeed(() => {
+                            memo_hooks.forEach((hook)=> hook.method(hook.element, hook.param, 'success'));
+                      })
+                      
+                      fail(() => {
+                            memo_hooks.forEach((hook)=> hook.method(hook.element, hook.param, 'fail'));
+                      })
+              })
+      }
+}
+// document.addEventListener('livewire:init', () => {
+
+//   const onLoadAddClass    = ( element, param, status ) => console.log( element,param,status)
+//   const onLoadRemoveClass = ( element, param, status ) => console.log( element,param,status )
+//    const directives = {
+//     'wr:loading.class' : onLoadAddClass,
+//     'wr:loading.class.remove' : onLoadRemoveClass
+//    };
+//    customDirectives.add('wr:loading.class', onLoadAddClass);
+//    customDirectives.add('wr:loading.class.remove', onLoadAddClass);
+//    customDirectives.init();
+// })
 
 
 $(document).ready( function() {
@@ -20,4 +94,6 @@ $(document).ready( function() {
            e.stopPropagation();
            $('#sidebar.active').removeClass('active');
      })
+
+    
 })
