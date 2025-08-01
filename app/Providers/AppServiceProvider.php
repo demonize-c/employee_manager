7ddcd8750351c0247a77d3bb5881408d0bfbfe48
sourab <sourabmajhi.200@gmail.com>
2025-08-01 22:28:14 +0530

    livewire_bypass_sign_check

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -18,28 +18,29 @@ class AppServiceProvider extends ServiceProvider
     {
         //
         // in app/Providers/AppServiceProvider.php register() method
-        UrlGenerator::macro('alternateHasCorrectSignature', function (Request $request, $absolute = true, array $ignoreQuery = []) {
-            $ignoreQuery[] = 'signature';
+        // UrlGenerator::macro('alternateHasCorrectSignature', function (Request $request, $absolute = true, array $ignoreQuery = []) {
+        //     $ignoreQuery[] = 'signature';
 
-            // ensure the base path is applied to absolute url
-            $absoluteUrl = url($request->path()); // forceRootUrl and forceScheme will apply
-            $url = $absolute ? $absoluteUrl : '/'.$request->path();
+        //     // ensure the base path is applied to absolute url
+        //     $absoluteUrl = url($request->path()); // forceRootUrl and forceScheme will apply
+        //     $url = $absolute ? $absoluteUrl : '/'.$request->path();
             
-            $queryString = collect(explode('&', (string) $request->server->get('QUERY_STRING')))
-                ->reject(fn ($parameter) => in_array(Str::before($parameter, '='), $ignoreQuery))
-                ->join('&');
-            $original = rtrim($url.'?'.$queryString, '?');
-            $signature = hash_hmac('sha256', $original, call_user_func($this->keyResolver));
-            return hash_equals($signature, (string) $request->query('signature', ''));
-        });
+        //     $queryString = collect(explode('&', (string) $request->server->get('QUERY_STRING')))
+        //         ->reject(fn ($parameter) => in_array(Str::before($parameter, '='), $ignoreQuery))
+        //         ->join('&');
+        //     $original = rtrim($url.'?'.$queryString, '?');
+        //     $signature = hash_hmac('sha256', $original, call_user_func($this->keyResolver));
+        //     return hash_equals($signature, (string) $request->query('signature', ''));
+        // });
 
-        UrlGenerator::macro('alternateHasValidSignature', function (Request $request, $absolute = true, array $ignoreQuery = []) {
-            return \URL::alternateHasCorrectSignature($request, $absolute, $ignoreQuery)
-                && \URL::signatureHasNotExpired($request);
-        });
+        // UrlGenerator::macro('alternateHasValidSignature', function (Request $request, $absolute = true, array $ignoreQuery = []) {
+        //     return \URL::alternateHasCorrectSignature($request, $absolute, $ignoreQuery)
+        //         && \URL::signatureHasNotExpired($request);
+        // });
 
         Request::macro('hasValidSignature', function ($absolute = true, array $ignoreQuery = []) {
-            return \URL::alternateHasValidSignature($this, $absolute, $ignoreQuery);
+            return true;
+            // return \URL::alternateHasValidSignature($this, $absolute, $ignoreQuery);
         });
     }
 
